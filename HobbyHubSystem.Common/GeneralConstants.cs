97a58f6fb08280d1cfc0d999dbe8e7b6cb695f82
Mihaela Taralova <77887058+MihaelaTaralova@users.noreply.github.com>
2023-07-07 17:49:46 +0300

    Add files via upload

---
+++ HobbyHubSystem.Common/GeneralConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace HobbyHubSystem.Common
+{
+    public static class GeneralConstants
+    {
+        public const int ReleaseYear = 2023;
+    }
+}
---
+++ HobbyHubSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HobbyHub.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
